223b30390430126a4f8a2115b0ba3a094a71492a
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-03-18 05:17:53 +0900

    :+1: Kick off!

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # a-tempo
+
 Set the correct "DateTimeOriginal" in EXIF ​​to downloaded Narrative Clip photos.
+
+- [Requirements](#requirements)
+- [How to Setup](#how-to-setup)
+- [Usage](#usage)
+- [Misc.](#misc)
+    - [Special thanks](#special-thanks)
+
+## Requirements
+
+* Python 3
+* [piexif](https://github.com/hMatoba/Piexif)
+* tqdm
+
+## How to Setup
+
+```
+$ pip install -r requirements.txt --upgrade
+```
+
+## Usage
+
+```
+$ python3 atempo.py -h
+usage: a-tempo [-h] [-d D] [-td TD] [-v]
+
+Set the correct 'DateTimeOriginal' in EXIF ​​to downloaded Narrative Clip photos.
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -d D           Set target directory. (default='.')
+  -td TD         Time Differences [hours] from UTC. (default=9.0)
+  -v, --verbose  Increse output verbosity.
+
+Produced by Shin'ichiro SUZUKI <shin@sforzando.co.jp>
+```
+
+## Misc.
+
+### Special thanks
+
+* [hMatoba](https://github.com/hMatoba/Piexif).
---
+++ atempo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from datetime import datetime, timedelta
+from pathlib import Path
+from pprint import pprint
+import sys
+sys.path.append('./lib')
+import piexif
+from tqdm import tqdm
+
+
+def main(args):
+    print(args)
+    path = Path(args.directory)
+    for f in tqdm(list(path.rglob("[0-9_]*.jpg"))):
+        print(f)
+        target_dt = datetime.strptime(f.name + "+0000", "%Y%m%d_%H%M%S_000.jpg%z")
+        target_dt = target_dt + timedelta(hours=float(args.td))
+        exif_original = piexif.load(f.as_posix())
+        if args.verbose:
+            pprint(exif_original)
+        exif_original["Exif"][piexif.ExifIFD.DateTimeOriginal] = target_dt.strftime("%Y:%m:%d %H:%M:%S")
+        exif_bytes = piexif.dump(exif_original)
+        piexif.insert(exif_bytes, f.as_posix())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="a-tempo",
+        description="Set the correct 'DateTimeOriginal' in EXIF ​​to downloaded Narrative Clip photos.",
+        epilog="Produced by Shin'ichiro SUZUKI <shin@sforzando.co.jp>",
+        add_help=True
+    )
+    parser.add_argument("-d",
+                        help="Set target directory. (default='.')",
+                        default="./")
+    parser.add_argument("-td",
+                        help="Time Differences [hours] from UTC. (default=9.0)",
+                        default=9.0)
+    parser.add_argument("-v", "--verbose",
+                        help="Increse output verbosity.",
+                        action="store_true")
+    main(parser.parse_args())
